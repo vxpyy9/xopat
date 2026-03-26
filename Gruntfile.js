777471563d4060ec368376ac9a6ad8e3bbad3383
Aiosa <horakj7@gmail.com>
2026-03-26 09:22:12 +0100

    fix: grunt import missing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+const BuildLogic = require("./server/utils/mixins/build-logic");
+
 module.exports = function(grunt) {
     // import utils first to initialize them
     require('./server/utils/grunt/utils')(grunt);
